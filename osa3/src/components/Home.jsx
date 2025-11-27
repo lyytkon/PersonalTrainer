87ac8269ce8fb01c9e910f33888c7f982f004523
Konsta Lyytikäinen <konsta.lyytikainen@gmail.com>
2025-11-27 11:53:16 +0200

    Lisätty etusivu (Home) ja projekti pilvessä

---
+++ osa3/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material';
+
+function Home() {
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" minHeight="60vh">
+      <Typography variant="h1">
+        Personal Trainer Sovellus
+      </Typography>
+    </Box>
+  );
+}
+
+export default Home;
